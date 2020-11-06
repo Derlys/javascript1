543514548354b74cd394ff8cf1dd7782c288c874
Derlys Dominguez <derlys275@gmail.com>
2020-11-06 11:23:16 -0500

    concept: variable (var)

--- Curso JS Moderno/02-Variables/js/01-app.js
+++ Curso JS Moderno/02-Variables/js/01-app.js
@@ -1,17 +1,36 @@
-// inicia una variable con su valor
-var producto = 'monitor';
-console.log (producto);
+// inicializar una variable con un valor
 
-producto = 'monitor curvo';
-console.log (producto);
+var producto = "Monitor de 27 pulgadas";
 
+console.log(producto);
+
+// las variables se pueden reasignar
+producto = "Monitor de 19 pulgadas";
+console.log(producto);
+
+//javascript es un lenguaje de tipo dinámico, no se especifica tipo de dato
 producto = 20;
-console.log (producto);
+console.log(producto);
+
+// Se pueden inicializar sin un valor y asignarlo después
 
 var disponible;
 disponible = true;
 disponible = false;
 
-var categoria = ' computadores',
-    marca = 'marca famosa',
-    calificacion = '5';
+// inicializar multiples variables
+
+var categoria = "computadoras",
+  marca = "marca famosa",
+  calificacion = 5;
+
+// camelCase
+var nombreProducto;
+
+//under course
+
+var nombre_producto;
+
+// PascalCase/utilizado en clases
+
+var NombreProducto;
